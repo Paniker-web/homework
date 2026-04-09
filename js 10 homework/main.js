737ff25c5c3ddfe92b0b74f4df038489dc00ddda
Paniker-web <koroliovdaniil@gmail.com>
2026-04-09 19:06:21 +0600

    Add files via upload

---
+++ js 10 homework/main.js
@@ -0,0 +1,12 @@
+const firstName = "Daniil";
+const lastName = "Korolev"
+const isStudent = true;
+const age = 17;
+const currentYear = 2026; 
+
+const birthYear = currentYear - age;
+console.log(birthYear)
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`);
+
+// Задача 4
+// Ответ: 123456789falsetrue, немного подсмотрел этот вариант т.к незнал что будет от пустой строки let e = Boolean (' ');
